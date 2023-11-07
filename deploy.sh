0d394b9aa1dd4208afa35c28c5d6d7d2f0d4d6d6
Bastien DELFORGE <bastien.delforge@worldline.com>
2023-11-07 14:05:55 +0100

    streamlit front-end (WIP)

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ export AZURE_OPENAI_EMBEDDING_MODEL="text-embedding-ada-002"
 export VERBA_WAIT_TIME_BETWEEN_INGESTION_QUERIES_MS="500"
 export VERBA_MODEL="gpt-4"
 export VERBA_URL="http://localhost:8080"
+export BASE_VERBA_API_URL="http://localhost"
 
 TENANT_NUMBER=$1
 #check that the tenant number is not empty and a number
@@ -17,11 +18,26 @@ then
 fi
 
 #set the VERBA_URL port as 8080+tenant number
-PORT=$(($TENANT_NUMBER+8000))
+VERBA_PORT=$(($TENANT_NUMBER+8000))
+STREAMLIT_PORT=$(($TENANT_NUMBER+8500))
 export WEAVIATE_TENANT='tenant_'$TENANT_NUMBER
 
+
 #start verba, store std and error logs in verba.$1.log, do not erase the previous logs
-verba start --port $PORT >> verba.$1.log 2>&1 
+echo "starting verba on port $VERBA_PORT..."
+# verba start --port $VERBA_PORT >> verba.$1.log 2>&1 &
 
+# Wait for verba to start
+sleep 1
 
+echo "verba started"
 
+echo "starting streamlit on port $STREAMLIT_PORT..."
+
+# Exécuter streamlit
+streamlit run streamlit_rag/app.py --server.port $STREAMLIT_PORT --server.headless true --theme.base dark --theme.primaryColor "4db8a7" -- --verba_port $VERBA_PORT --verba_base_url $BASE_VERBA_API_URL  >> streamlit.$1.log 2>&1 &
+
+# Wait for Streamlit to start
+sleep 1
+
+echo "Streamlit started"
